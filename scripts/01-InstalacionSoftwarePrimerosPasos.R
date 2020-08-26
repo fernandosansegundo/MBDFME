35be4a5303f99b2aa4f69756dd53d2e1b6dd3df2
fernandosansegundo <fernando.sansegundo@gmail.com>
2020-08-26 19:41:26 +0200

    Añadida información sobre otras herramientas (Docker, GitHub, Google Colab). Además se extrae el código de esta sesión a un script.

--- scripts/01-InstalacionSoftwarePrimerosPasos.R
+++ scripts/01-InstalacionSoftwarePrimerosPasos.R
@@ -1,4 +1,4 @@
-## ----eval = FALSE, comment = NULL----------------------------------------
+# Ejercicio 1  
 2 + 3
 15 - 7
 4 * 6
@@ -9,38 +9,21 @@ sqrt(26)
 sin(pi)
 sin(3.14)
 
-
-## ----eval = FALSE, comment = NULL,, comment = NULL-----------------------
-1.224606e-16
-
-
-## ----eval = FALSE, comment = NULL,, comment = NULL-----------------------
+# Ejercicio 2
 3 + 
 
-
-## ----eval = FALSE, comment = NULL,, comment = NULL-----------------------
 4/*3
 2/0
 sqrt(-4)
 
-
-## ----eval = FALSE, comment = NULL,, comment = NULL-----------------------
 Sqrt(4)
 
-
-
-
-## ----eval = FALSE, comment = NULL----------------------------------------
+# Variables y asignaciones
 a = 2
 
-
-
-
-## ----eval = FALSE, comment = NULL----------------------------------------
 a + 1
 
-
-## ----results='hide'------------------------------------------------------
+# Ejercicio 3
 a = 2
 b = 3
 c = a + b
@@ -48,130 +31,47 @@ a = b * c
 b = (c - a)^2
 c = a * b
 
-
-## ----comment=NULL--------------------------------------------------------
 (c = a + b)
 
-
-## ----eval = FALSE, comment = NULL----------------------------------------
 a = 2
 
-
-## ----eval = FALSE, comment = NULL----------------------------------------
 a <- 2
 
-
-
-
-
-
-
-
-## ----results='hide'------------------------------------------------------
-a = 4
-
-
-
-
-
-
-
-
-
-
-
-
-## ------------------------------------------------------------------------
+# Tablas (Data Frames)
 head(iris)
 
-
-## ----eval = FALSE, comment = NULL----------------------------------------
-View(iris)
-
-
-## ------------------------------------------------------------------------
-head(iris)
-
-
-## ------------------------------------------------------------------------
 dim(iris)
 
-
-## ------------------------------------------------------------------------
+# Selección de elementos de una tabla
 iris[2, 3]
 
-
-## ------------------------------------------------------------------------
 iris[2, 3] = 7
 head(iris)
 
-
-## ----eval = FALSE, comment = NULL----------------------------------------
 iris[ , 3]
-
-## ----echo=FALSE----------------------------------------------------------
 head(iris[ , 3], 38)
 
-
-## ----eval = FALSE, comment = NULL----------------------------------------
 iris$Petal.Length
-
-## ----echo=FALSE----------------------------------------------------------
 head(iris$Petal.Length, 38)
 
-
-## ------------------------------------------------------------------------
 iris[2, ]
 
-
-## ------------------------------------------------------------------------
 iris[49:52, c(1, 3, 5)]
 
-
-## ----eval = FALSE, comment = NULL----------------------------------------
 iris[iris$Sepal.Width > 2, ]
-
-## ----echo=FALSE----------------------------------------------------------
 head(iris[iris$Sepal.Width > 2, ])
 
-
-
-
-## ----message=FALSE-------------------------------------------------------
+# Instalación y carga de librerías
 library(tidyverse)
 
-
-## ----echo=FALSE, eval = FALSE, comment = NULL----------------------------
-library(nycflights13)
-View(flights)
-
-
-## ------------------------------------------------------------------------
 library(nycflights13)
 
-
-## ----echo=FALSE, eval = FALSE, comment = NULL----------------------------
-library(nycflights13)
-str(flights)
-library(tidyverse)
-ggplot(data = flights) + 
-  geom_boxplot(mapping = aes(origin, distance, color = origin)) 
-
-
-## ----message=FALSE, size="scriptsize"------------------------------------
+# Un primer encuentro con dplyr
 iris %>%
   select(c('Petal.Length', 'Petal.Width'))  %>%
   filter(Petal.Width > 2.3) 
 
-
-## ----eval = FALSE, comment = NULL----------------------------------------
-library(gapminder)
-View(gapminder)
-
-
-
-
-## ----echo=-1, comment=NULL, fig.align='center', out.width = "65%", size="scriptsize"----
+# Y un primer encuentro con ggplot
 library(gapminder)
 gapminder %>% 
   filter(year == 2007) %>% # Hasta aquí dplyr, ahora entra en acción ggplot
@@ -179,4 +79,3 @@ ggplot() +
   geom_point(mapping = aes(x = lifeExp, log(gdpPercap, 10), 
                            color = continent, size = pop)) +
   scale_size(range = c(.1, 24), name="Population (M)")
-
