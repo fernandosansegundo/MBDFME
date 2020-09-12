4277518a6462269b9fd4276be4410bf73026170e
fernandosansegundo <fernando.sansegundo@gmail.com>
2020-09-12 20:55:17 +0200

    Incorporo una solución con purl y gsub para las dos referencias a la carpeta de datos, la del Rmd y la del script que va al workdir.

--- scripts/sesion03-comandos.R
+++ scripts/sesion03-comandos.R
@@ -6,7 +6,7 @@
 require(gridExtra)
 require(tidyverse)
 require(haven)
-auto2 = read_dta("../datos/auto2.dta")
+auto2 = read_dta("./datos/auto2.dta")
 p1 = ggplot(auto2) + 
   geom_density(mapping = aes(price), color="red", fill="lightblue", size=1.5)
 
@@ -50,10 +50,12 @@ par(mfrow = c(1, 1))
 
 
 
+# Media aritmética, p. 9
 set.seed(2019)
 (muestra = sample(0:100, size = 20, replace = TRUE))
 (media = mean(muestra))
 
+# Valores atípicos, p. 10
 set.seed(2019)
 muestra = sample(0:100, size = 99, replace = TRUE) 
 (media = mean(muestra))
@@ -61,6 +63,7 @@ muestra = sample(0:100, size = 99, replace = TRUE)
 muestra2 = c(muestra, 1000)
 (media2 = mean(muestra2))
 
+# Mediana, p. 11
 set.seed(2019)
 (valores = sample(1:100, 17, replace = TRUE))
 
@@ -72,10 +75,13 @@ median(valores)
 median(muestra)
 median(muestra2)
 
+# Valores de posición, p. 13
 quantile(mpg$cty, probs = 0.43)
 
+# Summary, p. 13
 summary(mpg$cty)
 
+# El gráfico de dispersión de la página 14
 library(tidyverse)
 set.seed(2019)
 n = 400
@@ -93,10 +99,12 @@ ggplot(data = muestras) +
              position =  position_jitter(w = 0.05, h = 0)) + 
   coord_flip()
 
+# y la versión en curvas de densidad de la página 15
 plot(density(muestra1, adjust = 2), col="blue", 
      xlim = c(-12, 12), lwd = 2, main = "", xlab= "")
 lines(density(muestra2, adjust = 2), col="red", lwd = 2, sub="")
 
+# Recorrido intercuartílico, p. 16
 IQR(mpg$cty)
 
 summary(mpg$cty)
@@ -105,13 +113,16 @@ unname(quantile(mpg$cty, probs = c(1/4, 3/4)) + c(-1, 1) * 1.5 * IQR(mpg$cty))
 
 
 
+# Boxplot con R básico p. 19
 bxp_cty = boxplot(mpg$cty, col="orange")
 
 bxp_cty$out
 
+# Boxplot y violinplot con R básico p. 20
 ggplot(mpg) + 
   geom_boxplot(mapping = aes(y = cty), fill="orange") +
   scale_x_discrete(breaks = c())
+
 
 ggplot(mpg) + 
   geom_violin(mapping = aes(x=0, y = cty)) +
@@ -120,10 +131,12 @@ ggplot(mpg) +
   geom_jitter(aes(x=0, y = cty), 
               position = position_jitter(w=0.05, h= 0), col="blue")
 
+# Desviación absoluta mediana p. 21
 library(readxl)
-accidentes = read_excel("../datos/train_acc_2010.xls")
+accidentes = read_excel("./datos/train_acc_2010.xls")
 mad(accidentes$Speed, constant = 1)
 
+# Varianza y desviación típica p. 24
 var(mpg$displ)
 
 n = length(mpg$displ)
@@ -134,13 +147,121 @@ sd(mpg$displ)
 
 sqrt(var(mpg$displ))
 
+# Factores p. 26
 table(accidentes$TrkType)
 prop.table(table(accidentes$TrkType))
 
+# Gráficos para factores, p. 27
 library(viridisLite)
 barplot(prop.table(table(accidentes$TrkType)), col=viridis(5))
 ggplot(accidentes) + 
   geom_bar(mapping = aes(x = TrkType), fill= viridis(5))
 
-fhs = read_csv("../datos/framingham.csv")
+# Factores dicotómicos
+fhs = read_csv("./datos/framingham.csv")
 mean(fhs$male)
+
+#########################################################
+## Sección 4. Complementos de R
+## Listas, bucles, funciones, datos ausentes, Rmarkdown
+#########################################################
+
+(planeta = list(nombre = "Marte", exterior = TRUE, 
+                 radio = 3389.5, satelites = list("Fobos", "Deimos")))
+
+planeta[[1]]
+planeta$exterior
+planeta$satelites[[1]]
+
+planeta[1]
+planeta["exterior"]
+
+(l1 = list("A", "B"))
+(l2 = list(c("A", "B")))
+
+## (l3 = list(l2, "C"))
+
+l4 = append(l2, "D")
+(l4 = c(l2, "D"))
+
+l4[3] = NULL
+l4
+
+unlist(l1)
+
+## lista = list(letters[1:3], matrix(1:12, nrow = 3), TRUE)
+## unlist(lista)
+
+ifelse(((1:5) < 3), yes = "A",  no = "B")
+
+## valores = numeric(10) # Creamos un vector del tamaño previsto
+## for (k in 1:10){
+##   sorteo = sample(1:20, 1)
+##   print(paste0("k = ", k, ", sorteo = ", sorteo))
+##   if (k %in% 5:6){
+##     next # saltamos dos valores
+##   } else if (sorteo  == 1){
+##     print("Resultado del sorteo es 1, fin del bucle")
+##     break # paramos si un valor aleatorio es 1
+##   }
+##   valores[k] = k # se ejecuta cuando no se cumplan las condiciones
+## }
+## valores
+
+## # Ejemplo de bucle break
+## k = 0
+## while (k < 4){
+##   k = k + 1
+##   print(k)
+##   if(sample(1:6, 1) == 6){
+##     print("Final prematuro")
+##     break()
+##   }
+## }
+
+## # Ejemplo de bucle repeat similar al bucle while previo
+## k = 1
+## repeat {
+##   k = k + 1
+##   print(k)
+##   if(sample(1:6, 1) == 6){
+##     print("Final prematuro")
+##     break()
+##   }
+## }
+
+genPasswd = function(size, upp = TRUE, low = TRUE, nmb = TRUE){
+
+  # El vector pool guarda el juego de caracteres del password
+  pool = character() 
+  
+  # Generamos pool según las opciones
+  if(upp) pool = c(pool, LETTERS) 
+  if(low) pool = c(pool, letters)
+  if(nmb) pool = c(pool, 0:9)
+  
+  # Sorteamos los símbolos que aparecen en el password
+  passwd = sample(pool, size, replace = TRUE) 
+  # Y lo reducimos a un string con paste
+  paste(passwd, sep = "", collapse = "") 
+}
+
+genPasswd(size = 15)
+
+formals(genPasswd)
+
+body(genPasswd)
+
+body(genPasswd) = "No me apetece trabajar..."
+genPasswd(12)
+
+x = c(2, 3, -5, NA, 4, 6, NA)
+is.na(x)
+
+any(is.na(fhs$glucose))
+
+head(complete.cases(fhs), 17)
+
+mean(fhs$glucose)
+
+mean(fhs$glucose, na.rm = TRUE)
