1e61a04638edcd641f2b2909255166ee5621e193
fernandosansegundo <fernando.sansegundo@gmail.com>
2020-09-19 17:09:29 +0200

    Corrigiendo y completando el formato del código de la sesión 4

--- scripts/sesion04-comandos.R
+++ scripts/sesion04-comandos.R
@@ -3,20 +3,16 @@
 ## subtitle: "Sesión 4. Poblaciones, muestras y probabilidad. Variables aleatorias."
 
 ######################################################################
+# Población y muestra. 
 ######################################################################
-## ATENCIÓN: este fichero es una versión preliminar y posiblemente
-## incompleta (2020-09-17) del código de esta sesión. 
-######################################################################
-######################################################################
-
-
 
 ## Poblaciones y muestras aleatorias simples con vectores usando R.
 set.seed(2019)
 N = 158000
 poblacion = as.integer(2 * rchisq(N, df = 13), 0)
 
-
+hist(poblacion, main="", col="orange")
+abline(v = mean(poblacion), lty=2, lwd=5, col="blue")
 
 ## Medias muestrales
 options(width= 90)
@@ -61,18 +57,14 @@ mediasMuestrales = replicate(k, {
   mean(muestra)
 })
 
-
-
 #####################################################################
 # Otras poblaciones por si quieres experimentar
 #####################################################################  
 # Uniforme continua
 
-
 tamPoblacion = 100000
 poblacion = runif(tamPoblacion, min = 0, max = 10)
 head(poblacion, 100)
-
 
 hist(poblacion)
 plot(density(poblacion))
@@ -114,6 +106,10 @@ mean(mediasMuestrales)
 sd(mediasMuestrales)
 desvTipPob / sqrt(Tmuestra)
 
+######################################################################
+# Probabilidad básica.
+######################################################################
+
 ## La paradoja del cumpleaños.
 n = 366 # Número de personas en la sala
 
@@ -124,8 +120,6 @@ fechas = sort(sample(1:366, n, replace=TRUE))
 max(table(fechas)) # si el máximo es mayor que 1 es que 2 fechas coinciden
 })
 mean(pruebas > 1) # ¿qué proporción de salas tienen coincidencias?
-
-
 
 # Librería spam de Kernlab y Teorema de Bayes
 library(kernlab)
@@ -144,14 +138,17 @@ spam = spam %>%
 
 table(spam$hasOrder, spam$type)
 
+######################################################################
+# Variables aleatorias discretas.
+######################################################################
+
 muestra = sample(0:3, size = 10, replace = TRUE, prob = c(64, 48, 12, 1))
 
 library(viridisLite)
 muestra = sample(0:3, size = 1000, replace = TRUE, prob = c(64, 48, 12, 1))
 barplot(table(muestra), col=viridis(4))
 
-
-
+## Variable aleatoria binomial.
 library(tidyverse)
 fhs = read_csv("./datos/framingham.csv")
 tablaHyp = prop.table(table(fhs$prevalentHyp))
@@ -168,6 +165,7 @@ X = replicate(N, {
 prop.table(table(X))
 dbinom(x = 0:n, size = n, prob = p)
 
+## La binomial con R.
 dbinom(x = 3, size = 7, prob = p)
 
 signif(dbinom(x = 0:7, size = 7, prob = p), digits = 3)
@@ -176,16 +174,20 @@ signif(pbinom(q = 0:7, size = 7, prob = p), digits = 3)
 
 rbinom(n = 25, size = 7, prob = p)
 
+## Representación gráfica de la variable binomial.
 probabilidades = dbinom(x = 0:7, size = 7, prob = p)
 bp = barplot(probabilidades, space = 0, col="tan", names.arg = 0:7)
 arrows(seq(0.5, 7.5, by = 1), 0, seq(0.5, 7.5, by = 1), prop.table(table(X)), col="red", lwd = 2)
 
+## El zoo de las binomiales.
 probabilidades = dbinom(x = 0:12, size = 10, prob = 2/3)
 bp = barplot(probabilidades, space = 0, col="tan", names.arg = 0:12)
 
+## Binomiales con $n$ grande y $p$ moderado.
 probabilidades = dbinom(x = 0:100, size = 100, prob = 1/3)
 bp = barplot(probabilidades, space = 0, col="tan", names.arg = 0:100)
 
+## Cálculos de probabilidad en binomiales con $n$ muy grande.
 probabilidades = dbinom(x = 0:100, size = 100, prob = 1/3)
 barplot(probabilidades, space = 0, col="tan", names.arg = 0:100)
 par(new = T)
@@ -193,6 +195,7 @@ probabilidades[0:25] = 0
 probabilidades[36:100] = 0
 barplot(probabilidades/4, space = 0, col="blue", names.arg = 0:100)
 
+## Otra vez la discusión "*discreto frente a continuo*".
 probabilidades = dbinom(x = 0:100, size = 100, prob = 1/3)
 barplot(probabilidades, space = 0, col="tan", names.arg = 0:100)
 par(new = T)
@@ -200,39 +203,35 @@ probabilidades[0:25] = 0
 probabilidades[36:100] = 0
 barplot(probabilidades/4, space = 0, col="blue", names.arg = 0:100)
 
+######################################################################
+# Variables aleatorias continuas.
+######################################################################
 
-
-
-
-
-
+## Distribuciones normales en R.
 pnorm(10.5, mean=10, sd=2)
 
   1 - pnorm(11, mean=10, sd=2)
   pnorm(11, mean = 10, sd = 2, lower.tail = FALSE)
 
-
-
+## Probabilidad de un intervalo con `pnorm`.
 pnorm(12, mean=10, sd=2) - pnorm(7, mean=10, sd=2)
 
-
+## Problema inverso de probabilidad. La función `qnorm`.
 qnorm(p = 1/3, mean = 10, sd=2)
 
-
-
-
+# La función rnorm
 set.seed(2019)
 x1 = rnorm(1000)
 y1 = rnorm(1000)
 ggplot(data.frame(x1, y1)) +
   geom_point(mapping = aes(x1, y1), col="red")
+
 x2 = runif(1000, min = -1, max = 1)
 y2 = runif(1000, min = -1, max = 1)
 ggplot(data.frame(x2, y2)) +
   geom_point(mapping = aes(x2, y2), col="blue")
 
-
-
+## Suma (y mezcla) de normales independientes.
 set.seed(2019)
 pob1 = rnorm(30000, mean = -3, sd = 1)
 pob2 = rnorm(30000, mean = 2, sd = 0.5)
@@ -244,7 +243,6 @@ plot(density(pobSuma, adjust = 1.6), main="", lwd=5, col="red", xlab="")
 ## Operaciones con factores, verbos de dplyr.
 #########################################################
 
-
 options(width = 70)
 (ardeida = factor(c("martinete", "garzaReal", "avetorillo", "garzaReal",
                     "cangrejera", "martinete", "martinete"), ))
@@ -254,10 +252,13 @@ options(width = 70)
                   "cangrejera", "martinete", "martinete"), 
 levels = c("garzaReal", "martinete",  "cangrejera", "avetorillo")))  
 
+## Más funciones que generan factores
 options(width = 70)
 gl(n = 3, k = 4, labels = c("piedra", "papel", "tijera"))
 
 gl(n = 3, k=1, length = 30, labels = c("piedra", "papel", "tijera"))
+
+## Matrices
 
 (M = matrix(1:36, nrow=4) )
 
@@ -266,6 +267,7 @@ gl(n = 3, k=1, length = 30, labels = c("piedra", "papel", "tijera"))
 dim(M) = c(3, 12)
 M
 
+## Operaciones con matrices.
 v = rep(c(1, 2, 3), each=6)
 cat(paste0("v = c(", paste0(v,collapse = ", "), ")"))
 
@@ -275,7 +277,6 @@ Mv = matrix(v, nrow=3, byrow = TRUE)
 #############################################
 ### Verbos de dplyr
 #############################################
-
 
 # dplyr: select.
 
@@ -294,9 +295,9 @@ gapminder %>%
   select(starts_with("c")) %>% 
     names()
 
-  # dplyr: filter
-  
-  gapminder %>% 
+# dplyr: filter
+
+gapminder %>%
   filter(country == 'Spain') %>%
   head(4)
 
@@ -320,7 +321,6 @@ gapminder %>%
 # dplyr: summarise 
     iris %>% 
       summarise(mediana = median(Petal.Length), desvMediana = mad(Petal.Length))
-
 
 # dplyr: summarise con group_by
     
